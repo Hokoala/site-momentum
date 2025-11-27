d58e511710dba1783d5fcb0a1e5de48fa87f0cdc
Hokoala <jean-michel.le@etudiant.univ-reims.fr>
2025-11-27 10:44:40 +0100

    docs : update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,6 @@ export default function Header() {
                         alt="Momentum Logo"
                         width={32}
                         height={32}
-                        priority
                         className="w-10 h-auto"
                     />
                 </Link>
